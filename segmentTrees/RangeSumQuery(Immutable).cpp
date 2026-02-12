2326c794849cb3c7f6da6a4117bf907d8be0997a
Rahul Kumar <raahullkumar02@gmail.com>
2026-02-12 21:01:55 +0530

    practice problem

---
+++ segmentTrees/RangeSumQuery(Immutable).cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class NumArray {
+public:
+    vector<int> T;
+    int n;
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        T.resize(4*n);
+        build(nums,0,0,n-1);
+    }
+    
+    void build(vector<int> &nums,int i,int l,int r){
+        if(l==r){
+            T[i] = nums[l];
+            return;
+        }
+        int mid = (r-l)/2 + l;
+        build(nums,2*i+1,l,mid);
+        build(nums,2*i+2,mid+1,r);
+        T[i] = T[2*i+1] + T[2*i+2];
+        return;
+    }
+    int sum(int s,int e,int i,int l,int r){
+        if(l>e || r<s) return 0;
+        if(l>=s && r<=e) return T[i];
+        int mid = (r-l)/2 + l;
+        return sum(s,e,2*i+1,l,mid) + sum(s,e,2*i+2,mid+1,r);
+    }
+    int sumRange(int left, int right) {
+        return sum(left,right,0,0,n-1);
+    }
+};
